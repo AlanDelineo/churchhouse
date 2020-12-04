676dc977a666f45c8391ae50bb06b1a8201bc8a9
alan.huxley@delineo.com <alan.huxley@delineo.com>
2020-12-04 18:05:12 +0000

    Added correct 3d models, updated styles, looking much better

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -25,6 +25,52 @@ hallway.addEventListener('click', () => {
 	window.location.hash = 'lightbox';
 });
 
-// verticalCam.addEventListener('click', () => {
-// 	modelViewer.cameraOrbit="0.78deg 133deg 3134m";
-// });
+const model = document.querySelector("model-viewer");
+
+verticalCam.addEventListener('click', () => {
+	model.cameraOrbit="0.4116deg 25.74deg 3618m";
+});
+
+startCam.addEventListener('click', () => {
+  model.cameraOrbit="43.79deg 68.31deg 4008m";
+});
+
+viewAll.addEventListener('click', () => {
+  var hotspots = document.querySelectorAll(".Hotspot");
+  hotspots.forEach(function(hotspot){
+      hotspot.classList.remove("visible");
+  })
+});
+viewFloor1.addEventListener('click', () => {
+  var hotspots = document.querySelectorAll(".Hotspot");
+  hotspots.forEach(function(hotspot){
+      hotspot.classList.remove("visible");
+  })
+  var visibleHotspots = document.querySelectorAll(".floor1Hotspot");
+  visibleHotspots.forEach(function(visibleHotspot){
+      visibleHotspot.classList.add("visible");
+  })
+});
+viewFloor0.addEventListener('click', () => {
+  var hotspots = document.querySelectorAll(".Hotspot");
+  hotspots.forEach(function(hotspot){
+      hotspot.classList.remove("visible");
+  })
+  var visibleHotspots = document.querySelectorAll(".floor0Hotspot");
+  visibleHotspots.forEach(function(visibleHotspot){
+      visibleHotspot.classList.add("visible");
+  })
+});
+
+window.switchSrc = (element, name) => {
+    const base = "static/img/" + name;
+    model.src = base + '.glb';
+    const slides = document.querySelectorAll(".slide");
+    slides.forEach((element) => {element.classList.remove("selected");});
+    element.classList.add("selected");
+  };
+
+ document.querySelector(".controls").addEventListener('beforexrselect', (ev) => {
+    // Keep slider interactions from affecting the XR scene.
+    ev.preventDefault();
+  });
